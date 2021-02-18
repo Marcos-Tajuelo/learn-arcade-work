64482e97a91d55045f765cd9d606ab16dba8a72e
Marcos <marcos.tajuelo@edu.uah.es>
2021-02-18 20:24:29 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("Hello World")
-print("Hola Mundo")
-print("Hello World")
-print("Hello \n World")
+print("I'd like to go for a walk \" but it is cold outside")
+print("And i have to program a little bit")
+print("Maybe i could do it tomorrow, \n i dont know")
+print("We'll see if it is possible")
